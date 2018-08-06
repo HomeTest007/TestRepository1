eef63dd786771d46dbf7cc033582c270a2e23d34
alextest007 <42128570+alextest007@users.noreply.github.com>
2018-08-05 23:19:20 -0400

    Add files via upload

---
+++ TestFile1.java
@@ -0,0 +1,32 @@
+
+    import jade.core.Agent;
+    import jade.core.behaviours.*;
+    
+    public class myAgent extends Agent 
+    {
+        protected void setup() 
+        {
+            addBehaviour( new myBehaviour( this ) );
+        }
+        
+        
+        class myBehaviour extends SimpleBehaviour 
+        {   
+            public myBehaviour(Agent a) { 
+                super(a);  
+            }
+            
+            public void action() 
+            {
+               //...this is where the real programming goes !!
+            }
+            
+            private boolean finished = false;
+            
+            public boolean done() {  
+                return finished;  
+            }
+            
+        } // ----------- End myBehaviour
+        
+    }//end class myAgent
